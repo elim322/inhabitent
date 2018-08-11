3b6879278fc1ba63c694e29c1202803a74c8404c
elim322 <einer.lim322@gmail.com>
2018-08-11 13:56:09 -0700

    added posts and footer html

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -13,6 +13,21 @@
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
+				<div class="contact">
+				<h1 class="contact-info">CONTACT INFO</h1>
+				<p>info@inhabitent.com</p>
+				<p>778-456-7891</p>
+				</div>
+				<div class="business">
+				<h1 class="business hours">BUSINESS HOURS</h1>
+				<p><strong>Monday-Friday:</strong> 9am to 5pm</p>
+				<p><strong>Saturday:</strong> 10am to 2pm</p>
+				<p><strong>Sunday</strong>Closed</p>
+				</div>
+				<div class="copyright">
+				<p>COPYRIGHT © 2018 INHABITENT</p>
+				</div> 
+
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
