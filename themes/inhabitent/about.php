bd95cfd9675b4479acd278736fcc9c7e8816ec72
elim322 <einer.lim322@gmail.com>
2018-08-13 22:36:02 -0700

    added about.php and scss updated fonts

---
+++ themes/inhabitent/about.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: About Page
+ * 
+ * The template for displaying about page.
+ *
+ * @package RED_Starter_Theme
+ */
+
+ get_header(); ?>
+
+
+ <?php get_footer;?>
+
+ 
--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -12,14 +12,24 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="contact">
 				<h1 class="contact-info">CONTACT INFO</h1>
-				<a class="email " href="mailto:info@inhabitent.com">info@inhabitent.com</a>
-				<p>778-456-7891</p>
+				<p>
+					<i class="far fa-envelope">
+						::before
+					</i> 
+				<a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
+				</p>
+				<p>
+					<i class="fa fa-phone">
+						::before
+					</i>
+				<a href="tel:5553434567891">778-456-7891</a>
+				</p>
 				</div>
 				<div class="business">
 				<h1 class="business-hours">BUSINESS HOURS</h1>
 				<p><strong>Monday-Friday:</strong> 9am to 5pm</p>
 				<p><strong>Saturday:</strong> 10am to 2pm</p>
-				<p><strong>Sunday</strong>Closed</p>
+				<p><strong>Sunday:</strong> Closed</p>
 				</div>
 				<div class="footer-logo">
 				<img src="<?php echo get_template_directory_uri() ?>/logos/inhabitent-logo-text.svg">
