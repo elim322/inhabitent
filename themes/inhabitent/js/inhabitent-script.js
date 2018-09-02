42f65cd97686b3bbfe63c37393fa98b193dabcb7
elim322 <einer.lim322@gmail.com>
2018-09-02 15:08:24 -0700

    added and divided scss into more partials

--- themes/inhabitent/js/inhabitent-script.js
+++ themes/inhabitent/js/inhabitent-script.js
@@ -1,6 +1,4 @@
 (function($){
-  
-	
   $('.site-header .main-navigation .icon-search').on('click', function(event) {
     event.preventDefault();
     console.log('click');
@@ -12,25 +10,6 @@
     $('.site-header .main-navigation .search-field').removeClass('show');
   });
 
-  // $('.site-header .reverse-header .main-navigation .icon-search').on('click', function(event) {
-  //   event.preventDefault();
-  
-  //   $('.site-header .reverse-header .main-navigation .search-field').addClass('show');
-  //   $('.site-header .reverse-header .main-navigation .search-field').focus(); 
-  // });
-
-  // $('.site-header .reverse-header .main-navigation .search-field').on('blur', function(){
-  //   $('.site-header .reverse-header .main-navigation .search-field').removeClass('show');
-  // });
-
-  
-
-
-  // try on click of the search icon to .focus on the input field and try adding another listener for a .blur event, e.g. when you focus on the input if that input is blurred ( loses focus ) then close the search form
-  // .focus
-  // .blur
-
- // $('input-field-class').on('blur', function(){})
 
 
 })(jQuery);
